9037ab0feba23e5ec4cae652fab993e37527f920
Mateo Vasco <macv9620@utp.edu.co>
2024-01-12 23:42:48 -0500

    Se crea test de integracion

--- src/test/java/macv/ghactions/cicd/CicdApplicationTests.java
+++ src/test/java/macv/ghactions/cicd/CicdApplicationTests.java
@@ -1,13 +1,27 @@
 package macv.ghactions.cicd;
 
+import macv.ghactions.cicd.entity.UserEntity;
+import macv.ghactions.cicd.service.UserService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class CicdApplicationTests {
+	@Autowired
+	private UserService userService;
 
 	@Test
-	void contextLoads() {
+	void usersQuantity() {
+		List<UserEntity> users;
+
+		users = userService.getAllUsers();
+		int usersQuantity = users.size();
+
+		Assertions.assertEquals(2, usersQuantity);
 	}
 
 }
